4f7ac4dd764046f1896aa3926419e8ae4c1f495c
Vladislav Gusselnikov <vladjl3@yahoo.com>
2023-02-04 01:45:33 +0600

    feat: implement autocomplete while searching

--- src/app/modules/weather-widget/components/weather-widget-search/weather-widget-search.component.ts
+++ src/app/modules/weather-widget/components/weather-widget-search/weather-widget-search.component.ts
@@ -1,21 +1,45 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Observable,
+  Subject,
+  switchMap,
+} from 'rxjs';
+import { AutocompleteCityOption } from '../../interfaces/autocomplete-response.interface';
+import { WeatherData } from '../../interfaces/weather-data.interface';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-widget-search',
   templateUrl: './weather-widget-search.component.html',
   styleUrls: ['./weather-widget-search.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WeatherWidgetSearchComponent implements OnInit {
   city = '';
+  weather$!: Observable<WeatherData>;
+  cities$!: Observable<AutocompleteCityOption[]>;
   private searchSubject = new Subject<string>();
 
-  constructor() {}
+  constructor(private weatherService: WeatherService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cities$ = this.searchSubject.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((query) => {
+        console.log('query: ', query);
+        return this.weatherService.fetchAutocompleteOptions(query);
+      })
+    );
+  }
 
   public search() {
     this.searchSubject.next(this.city);
+  }
+
+  public selectCity(cityName: string) {
+    this.city = cityName;
+    console.log(this.city);
   }
 }
