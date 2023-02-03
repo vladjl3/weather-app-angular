02171f31bae9249ba21562181f6b38e6ca07b4bd
Vladislav Gusselnikov <vladjl3@yahoo.com>
2023-02-03 14:21:37 +0600

    feat: add core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [BrowserModule],
+  exports: [HeaderComponent],
+})
+export class CoreModule {}
