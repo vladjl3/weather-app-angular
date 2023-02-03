0e2ccc0613b4d89d83940f6c79b736f79f5b4942
Vladislav Gusselnikov <vladjl3@yahoo.com>
2023-02-03 18:27:04 +0600

    feat: do weather widget city search field layout

--- src/app/modules/weather-widget/components/weather-widget-search/weather-widget-search.component.ts
+++ src/app/modules/weather-widget/components/weather-widget-search/weather-widget-search.component.ts
@@ -1,16 +1,21 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-weather-widget-search',
   templateUrl: './weather-widget-search.component.html',
   styleUrls: ['./weather-widget-search.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WeatherWidgetSearchComponent implements OnInit {
+  city = '';
+  private searchSubject = new Subject<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public search() {
+    this.searchSubject.next(this.city);
   }
-
 }
--- src/app/modules/weather-widget/weather-widget.module.ts
+++ src/app/modules/weather-widget/weather-widget.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { WeatherWidgetComponent } from './container/weather-widget.component';
 import { WeatherWidgetSearchComponent } from './components/weather-widget-search/weather-widget-search.component';
 import { WeatherWidgetDataComponent } from './components/weather-widget-data/weather-widget-data.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [WeatherWidgetComponent, WeatherWidgetSearchComponent, WeatherWidgetDataComponent],
-  imports: [CommonModule],
+  declarations: [
+    WeatherWidgetComponent,
+    WeatherWidgetSearchComponent,
+    WeatherWidgetDataComponent,
+  ],
+  imports: [CommonModule, FormsModule],
   exports: [WeatherWidgetComponent],
 })
 export class WeatherWidgetModule {}
